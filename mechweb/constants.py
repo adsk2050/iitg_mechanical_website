a7e9fcf9cd68007bb9ba2fa4f9a8818e5b62b68e
Anmol Deep <adsk2050@users.noreply.github.com>
2019-01-28 18:16:26 +0900

    Added homepage features, will start working on events page

---
+++ mechweb/constants.py
@@ -0,0 +1,7 @@
+TEXT_PANEL_CONTENT_TYPES=(
+	('mini_article', 'mini_article'),
+)
+
+LOCATIONS=(
+	('seminar_hall', 'seminar_hall'),
+)
--- mechweb/models.py
+++ mechweb/models.py
@@ -1,11 +1,55 @@
-from wagtail.core.models import Page
+
+from django.db import models
+from django import forms
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, FieldRowPanel
+from modelcluster.fields import ParentalKey
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+from .constants import TEXT_PANEL_CONTENT_TYPES, LOCATIONS
 
 
 class MechHomePage(Page):
-    intro = RichTextField(blank=True)
+    intro = RichTextField(blank=True) 
 
     content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
+        FieldPanel('intro', classname="full"),
+        InlinePanel('text_panels', label="Mini Articles"),
+        InlinePanel('gallery_images', label="Gallery Images"),
     ]
+
+    def get_context(self, request):
+	    # Update context to include only published posts, ordered by reverse-chron
+	    context = super().get_context(request)
+	    navlist = self.get_children().live().order_by('-first_published_at')
+	    context['navlist'] = navlist
+	    return context
+
+class HomeTextPanel(Orderable):
+    page = ParentalKey(MechHomePage, on_delete=models.CASCADE, related_name='text_panels')
+    title = models.CharField(blank=True, max_length=50)
+    description = models.CharField(blank=True, max_length=500)
+    date = models.DateTimeField()
+    #change the below content_type code to manage css accordingly
+    content_type=models.CharField(default="mini_article", choices=TEXT_PANEL_CONTENT_TYPES, max_length=50)
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('description'),
+        FieldPanel('date'),
+        FieldPanel('content_type'),
+    ]
+
+class MechHomePageGalleryImage(Orderable):
+    page = ParentalKey(MechHomePage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+
