4d9bf12b89d3dd38d73cab0816fd3ba6d405388c
Anmol Deep <adsk2050@users.noreply.github.com>
2019-02-10 19:27:33 +0900

    Implemented Research pages - Labs, Publications, Projects

--- mechweb/models.py
+++ mechweb/models.py
@@ -31,7 +31,7 @@ class MechHomePage(Page):
 	]
 
 	parent_page_types=[]
-	subpage_types=['EventHomePage', 'FacultyHomePage', 'StudentHomePage']
+	subpage_types=['EventHomePage', 'FacultyHomePage', 'StudentHomePage', 'ResearchHomePage']
 
 	def get_context(self, request):
 		# Update context to include only published posts, ordered by reverse-chron
@@ -151,6 +151,8 @@ class EventPageLink(Orderable):
 	]
 
 ######################################################
+# Can I make a generic class which covers all these repeatedly adding of data models needed to be written only once?
+######################################################
 class FacultyHomePage(Page):	
 	intro = RichTextField(blank=True)
 	# head_of_dept = models.ForeignKey('FacultyPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='head_of_hept')
@@ -243,6 +245,7 @@ class FacultyPage(Page):
 	# 	context['blogpages'] = blogpages
 	# 	return context
 
+#After implementing publications document in research section, I want to know if we need to implement this or somehow list the publications from that model directly
 class FacultyPublicationDocument(Orderable):
 	page = ParentalKey(FacultyPage, on_delete=models.CASCADE, related_name='publications')
 	document = models.ForeignKey(
@@ -343,12 +346,7 @@ class StudentProject(Orderable):
 	page = ParentalKey(StudentPage, on_delete=models.CASCADE, related_name='projects')
 	guide = models.ForeignKey('FacultyPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='guide')
 	co_guide = models.ForeignKey('FacultyPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='co_guide')
-	document = models.ForeignKey(
-		'wagtaildocs.Document', 
-		null=True, blank=True, 
-		on_delete=models.SET_NULL, 
-		related_name='+'
-	)
+	document = models.ForeignKey('wagtaildocs.Document', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
 	photo_1 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
 	photo_2 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
 	photo_3 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
@@ -429,9 +427,213 @@ class StaffPage(Page):
 	subpage_types=[]
 
 ######################################################
-# class ResearchHomePage(Page):
-# 	pass
+class ResearchHomePage(Page):
+	parent_page_types=['MechHomePage']
+	subpage_types=['ResearchLabPage', 'PublicationHomePage', 'ProjectHomePage']
 
-# class ResearchLabPage(Page):
-# 	name = models.CharField(max_length=100)
-# 	pass
+#------------------------------------------
+class ResearchLabPage(Page):
+	name = models.CharField(max_length=100)
+	faculty_incharge = models.ForeignKey('FacultyPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='faculty_incharge')
+	intro = models.CharField(max_length=250)
+	body = RichTextField(blank=True)
+	contact_number = models.CharField(max_length=20, blank=True)
+	address = models.CharField(max_length=100, blank=True)
+	photo_1 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+	photo_2 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+	photo_3 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+
+	content_panels = Page.content_panels + [
+		FieldPanel('name'),
+		PageChooserPanel('faculty_incharge'),
+		FieldPanel('contact_number'),
+		FieldPanel('address'),
+		FieldPanel('intro'),
+		FieldPanel('body'),
+		InlinePanel('students', label="Students"),
+		InlinePanel('equipment', label="Lab Equipments"),
+		InlinePanel('links', label="Related Links"),
+		MultiFieldPanel([
+			ImageChooserPanel('photo_1'), 
+			ImageChooserPanel('photo_2'), 
+			ImageChooserPanel('photo_3'),
+		], heading ="Featured Photos")
+	]
+
+	parent_page_types=['ResearchHomePage']
+	subpage_types=[]
+
+class LabEquipment(Orderable):
+	page = ParentalKey(ResearchLabPage, on_delete=models.CASCADE, related_name='equipment')
+	operator = models.ForeignKey('StaffPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='operator')
+	document = models.ForeignKey(
+		'wagtaildocs.Document', 
+		null=True, blank=True, 
+		on_delete=models.SET_NULL, 
+		related_name='+'
+	)
+	description = RichTextField(blank=True)
+	specifications = RichTextField(blank=True)
+	cost = models.FloatField(blank=True)
+	date_of_procurement = models.DateField(blank=True)
+	link = models.URLField(max_length=250, blank=True)
+	photo_1 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+	photo_2 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+	photo_3 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+	photo_4 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+	photo_5 = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+	panels = [
+		PageChooserPanel('operator'),
+		DocumentChooserPanel('document'),
+		FieldPanel('description'),
+		FieldPanel('link'),
+		FieldPanel('cost'),
+		FieldPanel('date_of_procurement'),
+		MultiFieldPanel([
+			ImageChooserPanel('photo_1'), 
+			ImageChooserPanel('photo_2'), 
+			ImageChooserPanel('photo_3'),
+			ImageChooserPanel('photo_4'),
+			ImageChooserPanel('photo_5'),
+		], heading ="Featured Photos")
+		 
+	]
+
+class ResearchLabPageLink(Orderable):
+	page = ParentalKey(ResearchLabPage, on_delete=models.CASCADE, related_name='links')
+	link = models.URLField(max_length=250)
+	panels = [
+		FieldPanel('link'),
+	]
+
+class ResearchLabStudents(Orderable):
+	page = ParentalKey(ResearchLabPage, on_delete=models.CASCADE, related_name='students')
+	student = models.ForeignKey('StudentPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='student')
+	research_statement = RichTextField()
+	panels = [
+		PageChooserPanel('student'),
+		FieldPanel('research_statement'),
+	]
+
+#------------------------------------------
+class PublicationHomePage(Page):
+	content_panels = Page.content_panels + [
+		#InlinePanel('publication')
+	]
+
+	parent_page_types=['ResearchHomePage']
+	subpage_types=['PublicationPage']
+
+class PublicationPage(Page):
+	#page = ParentalKey(PublicationHomePage, on_delete=models.PROTECT, related_name='publication')
+	document = models.ForeignKey(
+		'wagtaildocs.Document', 
+		null=True, blank=True, 
+		on_delete=models.SET_NULL, 
+		related_name='+'
+	)
+	name = models.CharField(max_length=100, blank=True)
+	abstract = RichTextField(blank=True)
+	photo = models.ForeignKey('wagtailimages.Image',null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+
+	# student = models.ForeignKey('StudentPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='student_pub')
+	# faculty = models.ForeignKey('FacultyPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='faculty_pub')
+
+	content_panels =  Page.content_panels + [
+		DocumentChooserPanel('document'),
+		FieldPanel('name'),
+		FieldPanel('abstract'),
+		ImageChooserPanel('photo'),
+		# PageChooserPanel('student'),
+		# PageChooserPanel('faculty'),
+		InlinePanel('students', label="Students"),
+		InlinePanel('faculty', label="Faculty"),
+		InlinePanel('links', label="Links"),	
+	]
+
+class PublicationPageStudent(Orderable):
+	page = ParentalKey(PublicationPage, on_delete=models.CASCADE, related_name='students')
+	student = models.ForeignKey('StudentPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='student_pub')
+	research_statement = RichTextField()
+	panels = [
+		PageChooserPanel('student'),
+		FieldPanel('research_statement'),
+	]
+
+class PublicationPageFaculty(Orderable):
+	page = ParentalKey(PublicationPage, on_delete=models.CASCADE, related_name='faculty')
+	faculty = models.ForeignKey('FacultyPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='faculty_pub')
+	research_statement = RichTextField()
+	panels = [
+		PageChooserPanel('faculty'),
+		FieldPanel('research_statement'),
+	]
+
+class PublicationPageLink(Orderable):
+	page = ParentalKey(PublicationPage, on_delete=models.CASCADE, related_name='links')
+	link = models.URLField(max_length=250)
+	panels = [
+		FieldPanel('link'),
+	]
+
+#------------------------------------------
+class ProjectHomePage(Page):
+	parent_page_types=['ResearchHomePage']
+	subpage_types=['ProjectPage']
+
+class ProjectPage(Page):
+	principal_investigator = models.ForeignKey('FacultyPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='principal_investigator')
+	name = models.CharField(max_length=100)
+	start_date = models.DateField(blank=True)
+	end_date = models.DateField(blank=True)
+	budget = models.FloatField(blank=True)
+	funding_agency = models.CharField(max_length=100)
+	funding_agency_link = models.URLField(blank=True, max_length=100)
+	content_panels = Page.content_panels + [
+		FieldPanel('name'),
+		PageChooserPanel('principal_investigator'),
+		FieldPanel('budget'),
+		FieldPanel('start_date'),
+		FieldPanel('end_date'),
+		FieldPanel('funding_agency'),
+		FieldPanel('funding_agency_link'),
+		InlinePanel('students', label="Students"),
+		InlinePanel('faculty', label="Faculty"),
+		InlinePanel('links', label="Links"),
+		InlinePanel('gallery_images', label="Gallery images"),
+	]
+
+class ProjectPageFaculty(Orderable):
+	page = ParentalKey(ProjectPage, on_delete=models.CASCADE, related_name='faculty')
+	faculty = models.ForeignKey('FacultyPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='faculty_co_investigator')
+	project_statement = RichTextField()
+	panels = [
+		PageChooserPanel('faculty'),
+		FieldPanel('project_statement'),
+	]
+
+class ProjectPageStudent(Orderable):
+	page = ParentalKey(ProjectPage, on_delete=models.CASCADE, related_name='students')
+	student = models.ForeignKey('StudentPage', null=True,blank=True, on_delete=models.SET_NULL, related_name='faculty_co_investigator')
+	project_statement = RichTextField()
+	panels = [
+		PageChooserPanel('student'),
+		FieldPanel('project_statement'),
+	]
+
+class ProjectPageLink(Orderable):
+	page = ParentalKey(ProjectPage, on_delete=models.CASCADE, related_name='links')
+	link = models.URLField(max_length=250)
+	panels = [
+		FieldPanel('link'),
+	]
+
+class ProjectPageGalleryImage(Orderable):
+	page = ParentalKey(ProjectPage, on_delete=models.CASCADE, related_name='gallery_images')
+	image = models.ForeignKey( 'wagtailimages.Image', on_delete=models.CASCADE, related_name='+' )
+	caption = models.CharField(blank=True, max_length=250)
+	panels = [
+		ImageChooserPanel('image'),
+		FieldPanel('caption'),
+	]
+######################################################
