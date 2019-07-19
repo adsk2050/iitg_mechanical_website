9dc8c8268e07a5a3357ac0d2aeeceedd895b36ea
dhiraj1997 <mittal.dhiraj7@gmail.com>
2019-07-20 01:45:04 +0530

    filtering in staff

--- mechweb/models.py
+++ mechweb/models.py
@@ -1419,7 +1419,7 @@ class ProjectHomePage(Page):
 class ProjectPage(Page):
 	principal_investigator = models.ForeignKey('FacultyPage', null=True, blank=True, on_delete=models.SET_NULL, related_name='principal_investigator')
 	description = RichTextField(blank=True, features=CUSTOM_RICHTEXT)
-	name = models.CharField(max_length=75)
+	name = models.CharField(max_length=150)
 	start_date = models.DateField(default=timezone.now)
 	end_date = models.DateField(blank=True)
 	budget = models.CharField(blank=True, max_length=100)
