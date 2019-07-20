a62f69ce7db83a21e5bea23db190f64ffd279b99
Anmol Deep <adsk2050@users.noreply.github.com>
2019-07-20 16:43:18 +0530

    Update base.py

--- iitg_mechanical_website/settings/base.py
+++ iitg_mechanical_website/settings/base.py
@@ -163,11 +163,9 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
