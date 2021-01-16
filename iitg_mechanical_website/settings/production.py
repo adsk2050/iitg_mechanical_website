6100383d8ad683ac5f6e1ab37c6e90b78eb90e1a
Mechweb Admin <mechwebadmin@iitg.ac.in>
2021-01-16 21:06:46 +0530

    Data Update Jan 16, 2020, Fixed absolute URL problem for multiple hostnames

--- iitg_mechanical_website/settings/production.py
+++ iitg_mechanical_website/settings/production.py
@@ -5,20 +5,22 @@ except ImportError:
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['intranet.iitg.ac.in', 'intranet.iitg.ac.in/mech', '172.16.72.79']
+ALLOWED_HOSTS = ['intranet.iitg.ac.in', 'intranet.iitg.ac.in/mech', 'www.iitg.ac.in', 'www.iitg.ac.in/mech', 'iitg.ac.in', 'iitg.ac.in/mech', '172.16.72.79']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 
-BASE_URL = "intranet.iitg.ac.in"
+BASE_URL = "iitg.ac.in"
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 SECURE_SSL_REDIRECT=True
 SESSION_COOKIE_SECURE =True
 CSRF_COOKIE_SECURE=True
-CSRF_TRUSTED_ORIGINS = ['intranet.iitg.ac.in', 'intranet.iitg.ernet.in']
+CSRF_TRUSTED_ORIGINS = ['intranet.iitg.ac.in', 'iitg.ac.in','intranet.iitg.ernet.in']
 
 FIRST_DAY_OF_WEEK = 1
 
--- iitg_mechanical_website/strategy.py
+++ iitg_mechanical_website/strategy.py
@@ -1,12 +1,12 @@
 from social_django.strategy import DjangoStrategy
 
 class CustomDjangoStrategy(DjangoStrategy):
-    def build_absolute_uri2(self, path=None):
+    def build_absolute_uri(self, path=None):
         if self.request:
             return self.request.build_absolute_uri(path)
         else:
             return path
-
+"""
     def build_absolute_uri(self, path=None):
         if path:
             print(f"{path} -- in build_absolute_uri (custom)\n")
@@ -15,3 +15,4 @@ class CustomDjangoStrategy(DjangoStrategy):
             print("No path, running system defined build_absolute_uri\n")
             return "https://intranet.iitg.ac.in"
             # return self.build_absolute_uri2(path=path)
+"""
