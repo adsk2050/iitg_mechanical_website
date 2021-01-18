a6c2ac5463203b35ded9be525e2111d654d449de
Sudesh1122 <sudesh18@iitg.ac.in>
2021-01-18 06:30:45 +0530

    add custom template tag for breadcrumbs

--- mechweb/templatetags/mechweb_tags.py
+++ mechweb/templatetags/mechweb_tags.py
@@ -1,6 +1,7 @@
 import datetime
 from django import template
 from django.template.defaultfilters import stringfilter
+from wagtail.core.models import Page
 
 register = template.Library()
 
@@ -28,3 +29,16 @@ def noscrape(value):
     else:
         return ""
 
+@register.inclusion_tag('templatetags/breadcrumbs.html', takes_context=True)
+def breadcrumbs(context):
+    self = context.get('self')
+    if self is None or self.depth <= 2:
+        # When on the home page, displaying breadcrumbs is irrelevant.
+        ancestors = ()
+    else:
+        ancestors = Page.objects.ancestor_of(
+            self, inclusive=True).filter(depth__gt=2)
+    return {
+        'ancestors': ancestors,
+        'request': context['request'],
+    }
